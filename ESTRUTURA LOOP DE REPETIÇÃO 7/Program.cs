439185f9a38a797537bb47928e607e48d58c0084
Guilherme Rodrigues <157857247+GuilhermeRodOliveira@users.noreply.github.com>
2024-08-22 18:07:33 -0300

    Adicionar arquivos de projeto.

---
+++ ESTRUTURA LOOP DE REPETIÇÃO 7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ESTRUTURA_LOOP_DE_REPETIÇÃO_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //7 - Escreva um programa que peça ao usuário para digitar uma palavra e, em seguida, imprima essa palavra de trás para frente usando um loop for.
+            Console.WriteLine("Digite a palavra: ");
+            string palavra = Console.ReadLine();
+
+            for (int i = palavra.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(palavra[i]);
+            }
+
+        }
+    }
+}
